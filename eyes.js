5a3fc1b8f672de96d016d65f4649c30021dd8572
Tim Huerne <76855940+Drenhat@users.noreply.github.com>
2021-03-23 17:50:51 -0400

    update
    The eye balls have now a random size and color

--- eyes.js
+++ eyes.js
@@ -4,9 +4,36 @@ document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 2; i++) {
       balls[i].style.left = x;
       balls[i].style.top = y;
       balls[i].transform = "translate(-" + x + ",-" + y + ")";
   }
 };
+
+//Create a random color
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+getRandomSize = () => {
+ return ((Math.random() * 50) + 30);
+}
+
+transformEye = () => {
+  //change the color and the size of the eye balls
+  for (let i = 0; i < 2; i++) {
+    let firstEye = balls[i];
+    firstEye.style.background = getRandomColor();
+    let size = getRandomSize();
+    firstEye.style.width = `${size}px`;
+    firstEye.style.height = `${size}px`;
+  }
+}
+
+transformEye()
